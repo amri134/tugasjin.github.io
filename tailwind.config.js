a675c0c2b2f7d4f9b4a44994144c6fe7e22e8064
amri134 <72139322+amri134@users.noreply.github.com>
2022-06-01 14:20:01 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#b91c1c",
+        secondary: "#64748b",
+        dark: "#111827",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
